0bbb93e0673dcd4ac8fe7a328ef9558da812d8ca
Jonathan Carlim <jonathancarlimdev@gmail.com>
2023-02-28 12:49:08 -0300

    first class: Position

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,7 +1,10 @@
 package app;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Position p1 = new Position(1, 3);
+        System.out.println(p1);
     }
 }
